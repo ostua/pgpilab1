0c4618718d57c83f39fce287cf1dde24c3f17f22
jamkami <33749521+jamkami@users.noreply.github.com>
2017-11-17 11:55:20 +0100

    Update fact.c
    num cambiado a numTemp

--- fact.c
+++ fact.c
@@ -4,12 +4,12 @@
 #include "calcfact.h"
 int main(int argc, char **argv)
 {
-  int num;
+  int numTemp;
   if (argc!=2)
   {
 	printf("uso: %s <numero>\n", argv[0]);
         exit(-1);
   }
-  num=atoi(argv[1]);
-  printf ("resultado: el factorial de [%d] es [%d].\n", num, calcfact(num));
+  numTemp=atoi(argv[1]);
+  printf ("resultado: el factorial de [%d] es [%d].\n", numTemp, calcfact(numTemp));
 }
